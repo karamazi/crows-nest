ac4ac7a5036a88cb1d7b3ca3ae5d1715dded2a1c
Zibi <zibi667@gmail.com>
2016-06-16 17:56:03 +0100

    Working on actual project (failing)

--- functional_tests.py
+++ functional_tests.py
@@ -7,13 +7,14 @@ DJANGO_ADDRESS = 'http://localhost:8000'
 class VisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_if_django_app_has_started(self):
+    def test_if_app_has_started(self):
         self.browser.get(DJANGO_ADDRESS)
-        self.assertIn('Django', self.browser.title)
+        self.assertIn('Armada', self.browser.title)
 
 if __name__ == "__main__":
     unittest.main()
