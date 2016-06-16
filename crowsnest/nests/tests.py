3698e7b5126983deeca4bebdfdcba8dec5a1a4ee
Zibi <zibi667@gmail.com>
2016-06-16 18:23:25 +0100

    Test whether html is returned. (failing)

--- crowsnest/nests/tests.py
+++ crowsnest/nests/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from nests.views import index
 
 
@@ -7,3 +9,9 @@ class IndexTest(TestCase):
     def test_root_url_resolves_to_index_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, index)
+
+    def test_index_returns_proper_html(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertTrue(response.content.endwith(b'</html>'))
