e65f8b0e0ab7b55d1d1e40808269c0e580e317e1
Zibi <zibi667@gmail.com>
2016-06-16 18:12:15 +0100

    First test that's actually valid. It's still stupid as hell. (failing)

--- crowsnest/nests/tests.py
+++ crowsnest/nests/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from nests.views import index
 
 
-class DummyTest(TestCase):
-    def test_crap(self):
-        self.assertEqual(1, 0)
+class IndexTest(TestCase):
+    def test_root_url_resolves_to_index_page_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func, index)
