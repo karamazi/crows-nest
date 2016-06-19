ba986719a63a27b2c4087acf3658a83fc50758e1
Zibi <zibi667@gmail.com>
2016-06-19 17:16:59 +0100

    Allow user to add new cluster (failing)

--- crowsnest/nests/views.py
+++ crowsnest/nests/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<html></html>")
+    return HttpResponse("""<html>
+    <head>
+    <title>Armada Crow's Nest</title>
+    </head>
+    </html>""")
 
--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,8 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-DJANGO_ADDRESS = 'http://localhost:8000'
+LOCAL_APP_ADDRESS = 'http://localhost:8000'
 
 
 class VisitorTest(unittest.TestCase):
@@ -12,9 +13,28 @@ class VisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_if_app_has_started(self):
-        self.browser.get(DJANGO_ADDRESS)
+    def test_if_user_can_add_new_cluster(self):
+        # User enters the web app
+        self.browser.get(LOCAL_APP_ADDRESS)
         self.assertIn('Armada', self.browser.title)
+
+        # User wants to add new cluster
+        input_box = self.browser.find_element_by_id("id_new_cluster")
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            "Cluster's address"
+        )
+        input_box.send_keys('http://localhost:8900')
+        input_box.send_keys(Keys.ENTER)
+
+        # Cluster is now displayed
+        clusters_list = self.browser.find_element_by_id('id_clusters')
+        rows = clusters_list.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'http://localhost:8900' for row in rows)
+        )
+        # User wants to add another cluster
+        self.fail("Finish the test")
 
 if __name__ == "__main__":
     unittest.main()
